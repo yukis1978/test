f2ecfc2d0756264cf5254eba6e1dfdee5e77ab48
Yukis <yukis@HAL-MBA.local>
2012-12-24 22:35:54 +0900

    [TSK48] Fizzbuzz 作った

---
+++ fizzbuzz.php
@@ -0,0 +1,12 @@
+<?php
+$val = $argv[1];
+
+if ($val % 3 == 0 && $val % 5 == 0) {
+  echo 'FizzBuzz';
+} elseif ($val % 2 == 0) {
+  echo 'Fiz';
+} elseif ($val % 5 == 0) {
+  echo 'Buzz';
+} else {
+  echo $val;
+}
