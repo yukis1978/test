70e6e74aab14d42209f51ac70bd6a9817f6b311b
Alice <Alice@test.com>
2012-12-24 22:41:15 +0900

    [TSK48] 飾り文字を足した

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -2,11 +2,11 @@
 $val = $argv[1];
 
 if ($val % 3 == 0 && $val % 5 == 0) {
-  echo 'FizzBuzz';
+  echo '*FizzBuzz*';
 } elseif ($val % 2 == 0) {
-  echo 'Fiz';
+  echo '*Fiz*';
 } elseif ($val % 5 == 0) {
-  echo 'Buzz';
+  echo '*Buzz*';
 } else {
-  echo $val;
+  echo "*$val*";
 }
